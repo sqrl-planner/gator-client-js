a70b290e63c6227b94b55c9974343e479ab9418e
Eamon Ma <m@eamonma.com>
2023-04-30 23:32:10 -0400

    first commit

---
+++ apis/index.ts
@@ -0,0 +1,5 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './BuildingsApi';
+export * from './CoursesApi';
+export * from './InstitutionsApi';
---
+++ models/index.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './Building';
+export * from './BuildingPage';
+export * from './Course';
+export * from './CoursePage';
+export * from './Institution';
+export * from './InstitutionPage';
+export * from './Nested';
+export * from './Section';
+export * from './SectionMeeting';
+export * from './WeeklyRepetitionScheduleDump';
